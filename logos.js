5a8029834ceba6a3e3cde003541ee037aa756ca7
MRcat77 <notthemrcat@gmail.com>
2021-09-01 12:20:10 +0200

    footer
    added link to footer

---
+++ logos.js
@@ -0,0 +1,11 @@
+$("#graphide").load("assets/logos/graphide.svg")
+$("#yessness").load("assets/logos/yessness.svg")
+$("#discord").load("assets/logos/discord.svg")
+$("#github").load("assets/logos/github.svg")
+$("#twitter").load("assets/logos/twitter.svg")
+
+
+
+
+
+
