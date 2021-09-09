e7b453631970ea2c2bc2f112afc2021df0318eaf
MRcat77 <71332009+MRcat77@users.noreply.github.com>
2021-09-09 22:02:48 +0200

    Fixing info
    i did shit and im done with tool bar FINALY

--- logos.js
+++ logos.js
@@ -4,6 +4,17 @@ $("#discord").load("assets/logos/discord.svg")
 $("#github").load("assets/logos/github.svg")
 $("#twitter").load("assets/logos/twitter.svg")
 
+window.onscroll = function () {
+///    console.log(window.pageYOffset)
+
+    if (window.pageYOffset > 800)
+    {
+        document.querySelector("#tool-bar").style.position = "fixed"
+    }else
+    {
+        document.querySelector("#tool-bar").style.position = "initial"
+    }
+}
 
 
 
