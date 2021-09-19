e65af941fa63af0cb36e8652b386fa70d9fadd7e
MRcat77 <71332009+MRcat77@users.noreply.github.com>
2021-09-19 03:51:23 +0200

    responsivenes
    it works on mobile now

--- skript.js
+++ skript.js
@@ -1,17 +1,17 @@
+///SVG logos that used to link socials
 $("#graphide").load("assets/logos/graphide.svg")
 $("#yessness").load("assets/logos/yessness.svg")
 $("#discord").load("assets/logos/discord.svg")
 $("#github").load("assets/logos/github.svg")
 $("#twitter").load("assets/logos/twitter.svg")
 
+///fixed tool bar at top of screen
 window.onscroll = function () {
-///    console.log(window.pageYOffset)
+///console.log(window.pageYOffset)
 
-    if (window.pageYOffset > 800)
-    {
+    if (window.pageYOffset > 800) {
         document.querySelector("#tool-bar").style.position = "fixed"
-    }else
-    {
+    } else {
         document.querySelector("#tool-bar").style.position = "initial"
     }
 }
